53919a05e5d2b460a8f7b4ed8c6f0a497df176fc
Alicehhhmm <a007aplum@gmail.com>
2025-04-29 16:18:01 +0800

    fix:  update viteCopyPlugin copy file

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         emptyOutDir: true,
         rollupOptions: {
             input: getMultiTypeInputs(),
-            plugins: [viteCopyPlugin(['files'])],
+            plugins: [viteCopyPlugin(['files', 'manual/**/*.{json}'])],
         },
     },
 })
